c64c4dfdb68a4f6fd93fac06bafb75b118ae4b07
ChinaCaipu <MK520VIP@163.com>
2016-04-08 22:22:03 +0800

    更改ListHandlerFactory.java为动态代理模式

--- ChinaCaipu/src/com/china/caipu/list/ListHandlerFactory.java
+++ ChinaCaipu/src/com/china/caipu/list/ListHandlerFactory.java
@@ -1,5 +1,9 @@
 package com.china.caipu.list;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 /**
  * 
  * @author {Mark Sir}
@@ -7,14 +11,35 @@ package com.china.caipu.list;
  *         2016-4-7
  */
 public final class ListHandlerFactory {
-	
+
 	/**
 	 * 可以使用代理模式
 	 * 
 	 * @return
 	 */
 	public static IListHandler getIListHandler() {
-		return new ListHandlerImpl();
+		IListHandler handler = (IListHandler) Proxy.newProxyInstance(
+				ClassLoader.getSystemClassLoader(),
+				new Class[] { IListHandler.class },
+				new ListHandlerInvocationHandler());
+		return handler;
 	}
-	
-}//end
+
+	private static class ListHandlerInvocationHandler implements
+			InvocationHandler {
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args)
+				throws Throwable {
+			// TODO Auto-generated method stub
+			return method.invoke(getObj(), args);
+		}
+
+		private static Object getObj() throws Exception {
+			Class<?> cls = ListHandlerImpl.class;
+			Object obj = cls.newInstance();
+			return obj;
+		}
+	}
+
+}// end
